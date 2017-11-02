4763371ec6d6c973b3e9082c9af6f674c82f486f
FrenchMajesty <verdi.kap@gmail.com>
2017-11-02 15:35:57 -0500

    Create js conf file
    They might be shared with other layouts so it’s better to have a file
    for it now.

---
+++ resources/views/partials/jsconfig.blade.php
@@ -0,0 +1,5 @@
+<script data-main="{{asset('js/app')}}" src="{{asset('js/lib/require.js')}}"></script>
+<script type="text/javascript">
+    // Application-wide configs
+    define('config', {})
+</script>
