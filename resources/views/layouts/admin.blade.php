e72d042310e4dcc1447b1dbf750fd511dd062526
FrenchMajesty <verdi.kap@gmail.com>
2017-11-02 21:40:00 -0500

    Include js config and remove conflicting plugins
    I will eventually remove all the plugins one by one but there are the
    ones conflicting with requirejs for the moment.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -121,18 +121,11 @@
     </div>
 
 </body>
-
+    @include('partials.jsconfig')
     <!--   Core JS Files   -->
     <script src="{{asset('/js/jquery-3.1.0.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('/js/bootstrap.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('/js/material.min.js')}}" type="text/javascript"></script>
-
-    <!--  Charts Plugin -->
-    <script src="{{asset('/js/chartist.min.js')}}"></script>
-
-    <!--  Notifications Plugin    -->
-    <script src="{{asset('/js/bootstrap-notify.js')}}"></script>
-
     <!--  Google Maps Plugin    -->
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 
