1360068e40cc8a367c9c71533750d54085bb53f8
FrenchMajesty <verdi.kap@gmail.com>
2017-11-03 10:01:55 -0500

    Create test suit for books features in the panel

---
+++ tests/Feature/Panel/BooksTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Panel;
+
+use Auth;
+use App\User;
+use App\Book;
+use Tests\TestCase;
+use Laravel\Dusk\Chrome;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BooksTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $admin;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->admin = factory(User::class)->create([
+            'account' => 'admin',
+            'rank' => 2,
+        ]);
+    }
+
+    /**
+     * Test the ability to add a desired book.
+     *
+     * @return void
+     */
+    public function testCanAddABook()
+    {
+        // Given I have an admin and a book to add
+        $book = factory(Book::class)->make();
+
+        // When a desired book is added
+        $response = $this->actingAs($this->admin)
+                    ->post('/admin/books/add', [
+                        'isbn' => $book->isbn,
+                        'price' => $book->price,
+                    ]);
+                    
+        // Then it should be created and exist in the database
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('books', [
+            'isbn' => $book->isbn,
+        ]);
+    }
+}
