ae9413e3cb23f019a4864d90e36d306686185db2
FrenchMajesty <verdi.kap@gmail.com>
2017-11-02 21:41:54 -0500

    Create module for the page to add desired books

---
+++ public/js/app/pages/admin/add-book.js
@@ -0,0 +1,52 @@
+/*
+    |--------------------------------------------------------------------------
+    |  Add Book Module
+    |--------------------------------------------------------------------------
+    |
+    | This module is for the JavaScript on the page to add a desired book.
+    |
+    */
+define(['jquery','app/handler/form','app/ui-mod/BS-Notify'], ($, handler, BSNotify) => { 
+	
+	const FormHandler = handler()
+
+	const module = {}
+		
+	/**
+	 * Initialize module
+	 * @return {Void} 
+	 */
+	module.init = () => {
+		bindUIEvents()
+	}
+
+	/**
+	 * Submit an AJAX request to the server to create a book
+	 * @param  {Event} e Submit event
+	 * @return {Void}   
+	 */
+	function handleCreateBook(e) {
+		FormHandler.handleSubmit(e)
+		.then(_ => {
+			e.target.reset()
+			BSNotify({
+				style: 'success',
+				icon: 'check',
+				message: 'Your book was successfully created!',
+				side: 'right',
+			})
+		})
+		.fail(res => FormHandler.displayErrors(e.target, res.responseJSON.errors))
+	} 
+
+	/**
+	 * Bind events to page DOM
+	 * @return {Void} 
+	 */
+	function bindUIEvents() {
+		$('form').on('submit', handleCreateBook)
+		$('form input').on('input keydown change', FormHandler.clearErrors)
+	}
+
+	module.init()
+})
