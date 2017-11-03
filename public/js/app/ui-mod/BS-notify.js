acd8a6033e41cdbc72944b09055de297f5f2e65a
FrenchMajesty <verdi.kap@gmail.com>
2017-11-02 21:41:34 -0500

    Create module for simple bootstrap notifications

---
+++ public/js/app/ui-mod/BS-notify.js
@@ -0,0 +1,36 @@
+/*
+    |--------------------------------------------------------------------------
+    |  Notification Module
+    |--------------------------------------------------------------------------
+    |
+    | This module is used for conveniently generate bootstrap notifications on 
+    | the platform.
+    |
+    */
+define(['jquery','bootstrap-notify'], ($) => { 
+	
+	/**
+	 * Module to generate bootstrap notifications
+	 * @param  {Object} options Notifications parameters
+	 * @property {String} options.icon Material icon on the notification
+	 * @property {String} options.message Content of the notification
+	 * @property {String} options.style Bootstrap-style color of the notification
+	 * @property {Number} options.timer Time on screen in milliseconds
+	 * @property {String} options.from Vertical alignment of the notification
+	 * @property {String} options.align Horizontal alignment of the notification
+	 * @return {Void}         
+	 */
+	return (options) => {
+		$.notify({
+			icon: options.icon || 'notifications',
+			message: options.message || '...',
+		}, {
+			type: options.style || 'info',
+			timer: options.timer || 4000,
+			placement: {
+				from: options.align || 'top',
+				align: options.side || 'left',
+			},
+		})
+	}
+})
