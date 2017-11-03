4f9c9d0994725823c47f2a4a6a6fa3fa323ebb34
FrenchMajesty <verdi.kap@gmail.com>
2017-11-02 21:39:18 -0500

    make Book Model soft deletable

--- app/Book.php
+++ app/Book.php
@@ -3,17 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
+	use SoftDeletes;
+
 	/**
 	 * The attributes that are mass assignable
 	 * @var array
 	 */
     protected $fillable = ['isbn','price'];
 
-    public function getStatusAttribute()
-    {
-    	return 'null';
-    }
+    /**
+     * The attributes that should be mutated to dates
+     * @var array
+     */
+    protected $dates = ['created_at','updated_at','deleted_at'];
 }
