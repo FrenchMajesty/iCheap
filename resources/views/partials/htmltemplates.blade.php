4c4558fee3e6bd6931775cbe6b8061ac7aae1803
FrenchMajesty <verdi.kap@gmail.com>
2017-11-09 12:38:28 -0600

    Add form element to edit-book template
    It’s easier to do it here then patch something ugly together with
    jQuery in the module of the page where it’s used.

--- resources/views/partials/htmltemplates.blade.php
+++ resources/views/partials/htmltemplates.blade.php
@@ -1,19 +1,24 @@
 <script data-template="edit-book" type="text/x-handlebars-template">
-	<div class="row">
-		{{ csrf_field() }}
-		<input type="hidden" name="id" value="@{{id}}">
-	    <div class="col-md-6">
-			<div class="form-group label-floating is-empty">
-				<label class="control-label">ISBN</label>
-				<input type="text" name="isbn" class="form-control" value="@{{isbn}}" required>
-			<span class="material-input"></span></div>
-	    </div>
-	    <div class="col-md-6">
-			<div class="form-group label-floating is-empty">
-				<label class="control-label">Price</label>
-				<input type="number" name="price" class="form-control" value="@{{price}}" step="any" required>
-			<span class="material-input"></span></div>
-	    </div>
-	</div>
+	<form id="edit-book" class="row" method="POST" action="{{route('admin.books.update.desired')}}">
+		<div class="row">
+			{{ csrf_field() }}
+			<input type="hidden" name="id" value="@{{id}}">
+		    <div class="col-md-6">
+				<div class="form-group label-floating is-empty">
+					<label class="control-label">ISBN</label>
+					<input type="text" name="isbn" class="form-control" value="@{{isbn}}" required>
+				<span class="material-input"></span></div>
+		    </div>
+		    <div class="col-md-6">
+				<div class="form-group label-floating is-empty">
+					<label class="control-label">Price</label>
+					<input type="number" name="price" class="form-control" value="@{{price}}" step="any" required>
+				<span class="material-input"></span></div>
+		    </div>
+		</div>
+		<div class="row">
+			<section class="error"></section>
+		</div>
+	</form>
 </script>
 <script data-template="name" type="text/x-handlebars-template"></script>
