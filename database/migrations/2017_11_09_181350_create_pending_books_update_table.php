e4da5056dbc91fa8ffce15c1d85ee5215007c544
FrenchMajesty <verdi.kap@gmail.com>
2017-11-09 12:37:48 -0600

    Create database migration
    For the table to store pending changes on books if there is an ongoing
    order on the given book.

---
+++ database/migrations/2017_11_09_181350_create_pending_books_update_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePendingBooksUpdateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pending_books_update', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('pending_deletion')->default(false);
+            $table->string('column')->nullable();
+            $table->string('new_value')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pending_books_update');
+    }
+}
