a872d2e0a00d4b79920b94b9d35b0df971da7b0f
FrenchMajesty <verdi.kap@gmail.com>
2017-11-10 22:40:45 -0600

    Create migration to create the orders table

---
+++ database/migrations/2017_11_11_035725_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('book_id');
+            $table->string('book_tracking');
+            $table->string('status')->default('AWAITING_SHIPPING');
+            $table->string('payment_tracking')->nullable();
+            $table->timestamp('received_at')->nullable();
+            $table->timestamp('payed_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
