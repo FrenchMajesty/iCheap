9f97cf1c16bb412e8508c35f3d82aca9e080b05b
FrenchMajesty <verdi.kap@gmail.com>
2017-11-27 18:47:45 -0600

    Re-design login page to have a register form too
    So that users are not deterred away when they think about having to do
    a lot of clicks and navigations to create an account.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,122 @@
-@extends('layouts.app')
+@section('pageTitle', 'Sign In')
+
+@extends('layouts.platform')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+<div class="single-product-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-1">
+                <div class="product-content-right">
+                    <div class="woocommerce">
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+                        <form action="{{route('register')}}" class="checkout" method="post">
+                            <div id="customer_details" class="col2-set">
+                                <div class="col-1">
+                                    <div class="woocommerce-billing-fields">
+                                        <h3>Register</h3>
+                                        <p>To use {{env('APP_NAME')}} and start buying and selling books right away you must have an account, so what are you waiting for? Sign up below!</p>
+                                        
+                                        {{ csrf_field() }}
+                                        <p class="form-row form-row-first validate-required">
+                                            <label>First Name</label>
+                                            <input type="text" value="{{old('firstname')}}" placeholder="Enter your first name here" name="firstname" class="input-text" required>
+                                        </p>
+                                        @if ($errors->has('firstname'))
+                                        <span class="help-block red-text">
+                                            <strong>{{ $errors->first('firstname') }}</strong>
+                                        </span>
+                                        @endif
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                        <p class="form-row form-row-last validate-required">
+                                            <label>Last Name </label>
+                                            <input type="text" value="{{old('lastname')}}" placeholder="Enter your last name" name="lastname" class="input-text" required>
+                                        </p>
+                                        @if ($errors->has('lastname'))
+                                        <span class="help-block red-text">
+                                            <strong>{{ $errors->first('lastname') }}</strong>
+                                        </span>
+                                        @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                        <p class="form-row form-row-last validate-required">
+                                            <label>Email</label>
+                                            <input type="email" value="{{old('new-email')}}" placeholder="Enter your last name" name="new-email" class="input-text full-width" required>
+                                        </p>
+                                        @if ($errors->has('new-email'))
+                                        <span class="help-block red-text">
+                                            <strong>{{ $errors->first('new-email') }}</strong>
+                                        </span>
+                                        @endif
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                        <p class="form-row form-row-last validate-required">
+                                            <label>Password</label>
+                                            <input type="password" value="{{old('password')}}" placeholder="Enter your password" name="password" class="input-text full-width" required>
+                                        </p>
+                                        @if ($errors->has('password'))
+                                        <span class="help-block red-text">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                        @endif
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                                        <p class="form-row form-row-last validate-required">
+                                            <label>Address</label>
+                                            <input type="text" value="{{old('address')}}" placeholder="Enter your full address here" name="address" class="input-text" required>
+                                        </p>
+                                        @if ($errors->has('address'))
+                                        <span class="help-block red-text">
+                                            <strong>{{ $errors->first('address') }}</strong>
+                                        </span>
+                                        @endif
+                                        <div class="clear"></div>
+                                        <p>By signing up on {{env('APP_NAME')}}, you attest that you have read the <a href="#">Terms &amp; Conditions of Use</a> and the <a href="#">Privacy Policy</a> and agree with their content.</p>
+                                        <button type="submit" class="button col-md-12">Sign up now</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </form>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                    </div>                       
+                </div>                    
+            </div>
+            <div class="col-md-5">
+                <div class="single-sidebar">
+                    <form id="login-form-wrap" class="login" method="POST" action="{{ route('login') }}">
+                        <h2>Login</h2>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
+                        <p>If you have an account, please enter your details to login below. If you are a new around, fill out the register section on the left.</p>
+
+                        {{ csrf_field() }}
+                        <p class="form-row form-row-first">
+                            <label for="username">Email</label>
+                            <input type="text" name="email" class="input-text full-width" value="{{ old('email') }}" placeholder="Enter your email" required>
+                        </p>
+                        @if ($errors->has('email'))
+                        <span class="help-block red-text">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
+                        
+                        <p class="form-row form-row-last">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="input-text full-width" placeholder="Enter your password" required>
+                        </p>
+                        <div class="clear"></div>
+                        <p class="form-row">
+                            <input type="submit" value="Login" name="login" class="button col-md-12">
+                            <label class="inline" for="rememberme">
+                                <input type="checkbox" name="rememberme" {{ old('remember') ? 'checked' : '' }}> Remember me 
+                            </label>
+                        </p>
+                        <p class="lost_password">
+                            <a href="{{ route('password.request') }}">Lost your password?</a>
+                        </p>
+
+                        <div class="clear"></div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
