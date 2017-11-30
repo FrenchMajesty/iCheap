8af28bf776b5a5ecd1fc4df93405f8da62e342e6
FrenchMajesty <verdi.kap@gmail.com>
2017-11-29 21:04:21 -0600

    Create table and model for books dimensions
    This information is very much needed for shipping and stuffs..

---
+++ app/Book/BookDimensions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Book;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BookDimensions extends Model
+{
+	use SoftDeletes;
+
+	/**
+	 * The attributes that are mass assignables
+	 * @var array
+	 */
+    protected $fillable = ['book_id','height','width','thickness','weight'];
+
+    /**
+     * The attributes that should be mutated to dates
+     * @var array
+     */
+    protected $dates = ['created_at','updated_at','deleted_at'];
+}
