9eae1756b4a15ebeb7a2346cd69cd0bd4637b31f
FrenchMajesty <verdi.kap@gmail.com>
2017-12-01 23:48:20 -0600

    Create addresses table & Address model
    To store the full address of users

---
+++ app/Model/Accounts/Address.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model\Accounts;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{	
+	/**
+	 * The attributes that are mass assignable
+	 * @var array
+	 */
+	protected $fillable = [
+		'user_id',
+		'address',
+		'address_2',
+		'city',
+		'state',
+		'zip',
+	];
+
+	/**
+	 * Get the user associated with this address model
+	 */
+	public function user()
+	{
+		return $this->belongsTo('App\User');
+	}
+}
