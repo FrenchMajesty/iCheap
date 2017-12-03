27b96354ca3da685cad7927c1f2ffb7412f33314
FrenchMajesty <verdi.kap@gmail.com>
2017-12-02 21:40:54 -0600

    Add the $date property to mutate this model's dates

--- app/Model/Shipping/Label.php
+++ app/Model/Shipping/Label.php
@@ -22,6 +22,12 @@ class Label extends Model
     ];
 
     /**
+     * The attributes that should be mutated to dates
+     * @var array
+     */
+    protected $dates = ['created_at','updated_at','deleted_at'];
+
+    /**
      * Get the order that belongs to this shipping label
      */
     public function order()
