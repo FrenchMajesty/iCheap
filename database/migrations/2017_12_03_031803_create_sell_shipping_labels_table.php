ddeb13a66d45fb775611956beb804ad32764956f
FrenchMajesty <verdi.kap@gmail.com>
2017-12-02 22:10:40 -0600

    Correct invalid table name in sell_shipping_labels migration

--- database/migrations/2017_12_03_031803_create_sell_shipping_labels_table.php
+++ database/migrations/2017_12_03_031803_create_sell_shipping_labels_table.php
@@ -35,6 +35,6 @@ class CreateLabelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('labels');
+        Schema::dropIfExists('sell_shipping_labels');
     }
 }
