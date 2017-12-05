54a9b189a061d858653c2a4568f93faeb1b92559
FrenchMajesty <verdi.kap@gmail.com>
2017-12-04 22:01:12 -0600

    Update module for account to scroll when password was updated
    I want the page to auto scroll to the password container when it’s
    updated or submitted so I updated this module to take in configuration
    options and to scroll when the form was interacted with.

--- public/js/app/pages/platform/account.js
+++ public/js/app/pages/platform/account.js
@@ -1,50 +1,69 @@
 /*
-    |--------------------------------------------------------------------------
-    |  Account Module
-    |--------------------------------------------------------------------------
-    |
-    | This module is for the user's account page where they can update their
-    | account informations, passwords, and see past and current orders.
-    |
-    */
+|--------------------------------------------------------------------------
+|  Account Module
+|--------------------------------------------------------------------------
+|
+| This module is for the user's account page where they can update their
+| account informations, passwords, and see past and current orders.
+|
+*/
 define(['jquery'], ($) => { 
-	const module = {}
+	
+	return (option) => {
+		const module = {
+			options: $.extend(true, {
+				passwordWasSubmitted: false,
+			},option),
+		}
 
-	/**
-	 * Initialize module
-	 * @return {Void} 
-	 */
-	module.init = () => {
-		bindUIEvents()
-	}	
-
-	/**
-	 * Update the orders displayed in the column based on the search
-	 * @param  {Event} e Input event
-	 * @return {Void}   
-	 */
-	function updateOrdersDisplayed(e) {
-		const value = e.target.value
-		const orders = $('[data-order]')
-
-		orders.each((i, element) => {
-			const el = $(element)
-
-			if(!el.text().match(new RegExp(value, 'i'))) {
-				el.hide()
-			}else {
-				el.show()
+		/**
+		 * Initialize module
+		 * @return {Void} 
+		 */
+		module.init = () => {
+			if(module.options.passwordWasSubmitted) {
+				scrollToPasswordContainer()
 			}
-		})
-	}
 
-	/**
-	 * Bind events to the page DOM
-	 * @return {Void} 
-	 */
-	function bindUIEvents() {
-		$('#search input').on('keyup', updateOrdersDisplayed)
-	}
+			bindUIEvents()
+		}	
 
-	module.init()
+		/**
+		 * Update the orders displayed in the column based on the search
+		 * @param  {Event} e Input event
+		 * @return {Void}   
+		 */
+		function updateOrdersDisplayed(e) {
+			const value = e.target.value
+			const orders = $('[data-order]')
+
+			orders.each((i, element) => {
+				const el = $(element)
+
+				if(!el.text().match(new RegExp(value, 'i'))) {
+					el.hide()
+				}else {
+					el.show()
+				}
+			})
+		}
+
+		/**
+		 * Scroll the page down to the container to update the user's password
+		 * @return {Void} 
+		 */
+		function scrollToPasswordContainer() {
+	        $('html, body').animate({ scrollTop: $('#login-form-wrap').offset().top }, 1000)
+		}
+
+		/**
+		 * Bind events to the page DOM
+		 * @return {Void} 
+		 */
+		function bindUIEvents() {
+			$('#search input').on('keyup', updateOrdersDisplayed)
+		}
+			
+		module.init()
+	}
 })
