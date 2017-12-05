8d2062f3c6cf6428df915e064a3f7ce1e566b0bb
FrenchMajesty <verdi.kap@gmail.com>
2017-12-04 21:24:20 -0600

    Use formatted Address in input on account page
    The address attribute of user returns the entire model so in this input
    I had the JSON of the Address Model…
    
    I created the formatted attribute for this reason so that the address
    components can be merged for displaying in inputs.

--- resources/views/platform/account.blade.php
+++ resources/views/platform/account.blade.php
@@ -90,7 +90,7 @@
                             <div class="clear"></div>
                             <p class="form-row form-row-wide">
                                 <label>Address</label>
-                                <input type="text" value="{{old('address') ?: $user->address}}" placeholder="Enter your mailing address" name="address" class="input-text" required>
+                                <input type="text" value="{{old('address') ?: $user->address->formatted}}" placeholder="Enter your mailing address" name="address" class="input-text" required>
                             </p>
                             @if ($errors->has('address'))
                             <span class="help-block red-text">
