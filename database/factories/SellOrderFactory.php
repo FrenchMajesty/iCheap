4ffa58692edd56e91f88669b10f6be4d26321f69
FrenchMajesty <verdi.kap@gmail.com>
2017-12-09 07:56:36 -0600

    Remove non existing attribute on SellOrder factory
    There isn’t a book_tracking column on the orders table anymore ( I
    created a whole table for it instead) so this is out of place.

--- database/factories/SellOrderFactory.php
+++ database/factories/SellOrderFactory.php
@@ -12,7 +12,6 @@ $factory->define(Order::class, function (Faker $faker) {
     return [
     	'user_id' => factory(User::class)->create()->id,
     	'book_id' => factory(Book::class)->create()->id,
-    	'book_tracking' => $faker->text(12),
     	'status_id' => factory(OrderStatus::class)->create()->id,
     ];
 });
