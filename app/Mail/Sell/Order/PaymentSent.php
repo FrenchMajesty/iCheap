8e086de6a9b9f4e71937192bb1d5a306fe0cff72
FrenchMajesty <verdi.kap@gmail.com>
2017-12-08 23:19:07 -0600

    Create the Mailable for the email notification when payment is sent out

---
+++ app/Mail/Sell/Order/PaymentSent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail\Sell\Order;
+
+use App\Model\Sell\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PaymentSent extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Order instance for which the payment is being sent
+     * @var Order
+     */
+    public $order;
+
+    /**
+     * Call to action button's informations
+     * @var array
+     */
+    public $button;
+
+    /**
+     * Create a new message instance
+     *            
+     * @param Order $order Order instance that was just paid out
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+
+        $this->button['url'] = $order->paymentShipping->tracking_url;
+        $this->button['message'] = 'Track my Payment';
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mail.order.payment-sent')
+                    ->subject('You\'ve got money coming your way!');
+    }
+}
---
+++ resources/views/mail/order/payment-sent.blade.php
@@ -0,0 +1,31 @@
+@component('mail::message')
+
+@component('mail::panel')
+
+# Yo {{$order->user->firstname}} we've got some good news!
+
+We have examined the book "*{{$order->book->title}}*" you sent us {{$order->created_at->diffForHumans()}} and our team appraised it and wrote you a check for it of **${{number_format($order->payment_amount, 2)}}**. 
+We are going to be sending that over to you in the next couple of days.
+
+To track the delivery, please click the button below.
+
+@if($button)
+@component('mail::button', ['url' => $button['url'], 'color' => 'blue'])
+{{$button['message']}}
+@endcomponent
+@endif
+
+If you cannot click that button just copy and paste this link in your browser window: <{{$order->paymentShipping->tracking_url}}>
+
+Thanks, 
+
+-The {{ env('APP_NAME') }} team
+
+<div class="italic"><br><br>
+Do not reply to this email; it was automatically generated.<br>
+
+For security reasons, {{env('APP_NAME')}} will never ask you for your password via email.
+</div>
+@endcomponent
+
+@endcomponent
