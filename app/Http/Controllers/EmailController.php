c401c2159f28ff5262df3a989782bb3a97103b06
FrenchMajesty <verdi.kap@gmail.com>
2017-12-09 11:52:13 -0600

    Create method in EmailController to handle requests of additional verification links

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Events\Accounts\RequestVerificationLink;
 use App\Model\Accounts\Registration\EmailVerificationToken;
 use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
+	/**
+	 * Handle request to re-send another verification link to the user
+	 * @param  \Illuminate\Http\Request $request Request
+	 * @return \Illuminate\Http\Response           
+	 */
+	public function sendVerificationEmail(Request $request)
+	{
+		$this->validate($request, [
+			'email' => 'required|email|max:255|exists:users',
+		]);
+
+		$user = User::where('email', $request->email)->first();
+		event(new RequestVerificationLink($user));
+
+		return back()->with('status', 'We just sent you another email! Please wait 5 minutes before requesting another link.'); 
+	}
+
     /**
 	 * Handle the validating of a user's email address
 	 * @param  \Illuminate\Http\Request $request Request
