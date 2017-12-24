c15d1075c8e8853b06ac8f03479cfa00ff0d5ed6
FrenchMajesty <verdi.kap@gmail.com>
2017-12-24 00:50:50 -0600

    Clean up HTML code of site layout
    Like removing HTML comments and fixing broken links.

--- resources/views/layouts/platform.blade.php
+++ resources/views/layouts/platform.blade.php
@@ -7,85 +7,35 @@
     @yield('meta')
     <title>@yield('pageTitle') - {{env('APP_NAME')}}</title>
     
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    
-    <!-- Custom CSS -->
+    <link href='//fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
-   
-    <div class="header-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="user-menu">
-                        <ul>
-                            <li><a href="#"><i class="fa fa-user"></i> My Account</a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
-                            <li><a href="cart.html"><i class="fa fa-user"></i> My Cart</a></li>
-                            <li><a href="checkout.html"><i class="fa fa-user"></i> Checkout</a></li>
-                            <li><a href="#"><i class="fa fa-user"></i> Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <div class="header-right">
-                        <ul class="list-unstyled list-inline">
-                            <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">currency :</span><span class="value">USD </span><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">USD</a></li>
-                                    <li><a href="#">INR</a></li>
-                                    <li><a href="#">GBP</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">language :</span><span class="value">English </span><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">English</a></li>
-                                    <li><a href="#">French</a></li>
-                                    <li><a href="#">German</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End header area -->
     
     <div class="site-branding-area">
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="index.html">i<span>Cheapbooks</span></a></h1>
+                        <h1><a href="{{route('index')}}">i<span>Cheapbooks</span></a></h1>
                     </div>
                 </div>
             </div>
         </div>
-    </div> <!-- End site branding area -->
+    </div>
     
     <div class="mainmenu-area">
         <div class="container">
@@ -104,8 +54,12 @@
                         <li {{ $route == 'index' ? 'class=active' : '' }}>
                             <a href="{{route('index')}}">Home</a>
                         </li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Contact Us</a></li>
+                        <!--li{{$route == 'about' ? 'class=active' : ''}}>
+                            <a href="#">About Us</a>
+                        </li-->
+                        <li {{$route == 'contact' ? 'class=active' : ''}}>
+                            <a href="{{route('contact')}}">Contact Us</a>
+                        </li>
                         @guest
                             <li {{ ($route == 'login' || $route == 'register')  ? 'class=active' : '' }}>
                                 <a href="{{route('login')}}">Sign Up/Login</a>
@@ -122,20 +76,23 @@
                 </div>  
             </div>
         </div>
-    </div> <!-- End mainmenu area -->
+    </div>
     
     @yield('content')
     
     <div class="footer-top-area" style="background: inherit">
         <div class="zigzag-bottom"></div>
-    </div> <!-- End footer top area -->
+    </div>
     
     <div class="footer-bottom-area">
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
                     <div class="copyright">
-                        <p>&copy; {{date('Y').' '.env('APP_NAME')}}. All Rights Reserved. Designed by <a href="http://design-by-verdi.com" target="_blank">Verdi Co.</a></p>
+                        <p>&copy; {{date('Y').' '.env('APP_NAME')}}. All Rights Reserved. Design by <a href="http://victron-tech.com" target="_blank">Victron Tech.</a> |
+                            <a href="#">Privacy Policy</a> |
+                            <a href="#">Terms and Conditions of Use</a>
+                        </p>
                     </div>
                 </div>
                 
@@ -149,12 +106,11 @@
                 </div>
             </div>
         </div>
-    </div> <!-- End footer bottom area -->
+    </div>
     
     @include('partials.htmltemplates')
     @include('partials.jsconfig')
     
-    <!-- Main Script -->
     <script type="text/javascript">
         window.onload = () => {
             setTimeout(() => {
